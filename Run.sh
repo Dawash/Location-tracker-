acb890559ca2483849870ecb46572f121111b3e8
Dawash <57761316+Dawash@users.noreply.github.com>
2023-08-02 01:58:51 +0800

    Create Run.sh

---
+++ Run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Step 1: Compile the C++ code
+g++ main.cpp -lboost_system -lboost_thread -pthread -o websocket_server
+
+# Step 2: Run the C++ WebSocket server in the background
+./websocket_server &
+
+# Step 3: Run the main.css interface file
+xdg-open main.css
+
+# Step 4: Wait for user input (Enter key press) to terminate the WebSocket server
+echo "Press Enter to stop the WebSocket server..."
+read
+killall websocket_server
