d785ead86378f06516b3df38adc9b344f8aac4af
Dawash <57761316+Dawash@users.noreply.github.com>
2023-08-02 01:45:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,5 +13,15 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    alert('Your current location:\nLatitude: ' + position.coords.latitude + '\nLongitude: ' + position.coords.longitude);
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    var locationLink = "https://example.com/location?lat=" + latitude + "&lon=" + longitude;
+    alert('Your current location:\nLatitude: ' + latitude + '\nLongitude: ' + longitude);
+    sendLocationLink(locationLink);
+}
+
+function sendLocationLink(link) {
+    // Code to send the location link to the server or other actions you want to perform with the link.
+    console.log("Location link sent: " + link);
 }
